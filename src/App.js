f184f7074c44208a0728d34bcd1a3673e0f4b5a8
bonilla_d <bonilla_douglas@ymail.com>
2026-06-03 21:51:54 -0700

    filter quotes by category

--- src/App.js
+++ src/App.js
@@ -9,33 +9,52 @@ function App() {
 
   const [quotes, setQuotes] = useState([])
   const [loading, setLoading] = useState(false)
+  const [category, setCategory] = useState("All")
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
 
-    const fetchQuotes = async() => {
-      try{
-        setLoading(true);
-        const response = await fetch (quotesUrl);
-        const results = await response.json();
-        setQuotes(results)
-      }catch (e){
-        console.log("There was an error",e)
-    
+
+  const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
+
+  const fetchQuotes = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(quotesUrl);
+      const results = await response.json();
+      setQuotes(results)
+    } catch (e) {
+      console.log("There was an error", e)
+
     }
     setLoading(false);
-    };
+  };
 
 
-    useEffect(()=> {
-      fetchQuotes();
-    }, []);
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
 
-    console.log(quotes);
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const filteredQuotes = category !== "All" ? quotes.filter(quote => quote.categories.includes(category)) : quotes;
 
   return (
     <div className='App'>
       <Header />
-      <main>{loading ? <Loader /> : <Quotes quotes={quotes} /> } </main>
+      <main>
+        {loading ? (
+          <Loader />
+        ) : (
+          <Quotes
+            filteredQuotes={filteredQuotes}
+            categories={categories}
+            category={category}
+            handleCategoryChange={handleCategoryChange}
+          />
+        )}
+      </main>
       <Footer />
     </div>
   );
--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -1,14 +1,19 @@
 import React from "react";
 import QuoteCard from "./Quotecard";
+import CategoryForm from "./CategoryForm";
 
-const Quotes = ({quotes}) => {
+const Quotes = ({ filteredQuotes, quotes, categories, category, handleCategoryChange, }) => {
     return (
         <section className='all-quotes'>
-            <div className='quotes wrapper'> 
-                {quotes.map((quote) => (
-                    <QuoteCard key={quote.id} quote={quote} />
-                ))}
+            <div className='quotes-wrapper'>
+                <div className='category-header'>
+                    <p> Browse through your collection of quotes </p>
+                    <CategoryForm categories={categories} category={category} handleCategoryChange={handleCategoryChange} />
 
+                    {filteredQuotes.map((quote) => (
+                        <QuoteCard key={quote.id} quote={quote} />
+                    ))}
+                </div>
             </div>
         </section>
     );
